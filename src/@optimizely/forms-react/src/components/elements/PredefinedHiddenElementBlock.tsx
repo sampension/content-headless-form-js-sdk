0a65caab7f0f45df82b8dda852f6af41cadf537e
EP\qang2 <quocanh.nguyen@episerver.com>
2023-10-17 17:31:29 +0700

    Add PredefinedHiddenElementBlock Fixes: AFORM-3559

---
+++ src/@optimizely/forms-react/src/components/elements/PredefinedHiddenElementBlock.tsx
@@ -0,0 +1,24 @@
+import { PredefinedHidden } from "@optimizely/forms-sdk"
+import React from "react";
+import ElementWrapper from "../ElementWrapper";
+import { useElement } from "../../hooks/useElement";
+
+export interface PredefinedHiddenElementBlockProps {
+    element: PredefinedHidden
+}
+
+export const PredefinedHiddenElementBlock = (props: PredefinedHiddenElementBlockProps) => {
+    const { element } = props;
+    const { elementContext, checkVisible } = useElement(element);
+
+    return (
+        <ElementWrapper className={`Form__Element FormHidden FormHideInSummarized`} isVisible={checkVisible()}>
+            <input
+                value={elementContext.value as string}
+                name={element.key}
+                id={element.key}
+                type="hidden"
+            />
+        </ElementWrapper>
+    );
+}
