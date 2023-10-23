6e791350133136f64076adac90c740cb791f6e71
EP\qang2 <quocanh.nguyen@episerver.com>
2023-10-23 11:01:40 +0700

    Add ImageChoiceElementBlock Fixes: AFORM-3556

--- src/@optimizely/forms-sdk/src/models/elements/ImageChoice.ts
+++ src/@optimizely/forms-sdk/src/models/elements/ImageChoice.ts
@@ -7,5 +7,14 @@ export interface ImageChoice extends InputElementBase {
 }
 
 export interface ImageChoiceProperties extends InputElementBaseProperties {
-    showSelectionInputControl: boolean
+    showSelectionInputControl: boolean,
+    allowMultiSelect: boolean
+    items: FeedImageItem[]
+}
+
+export interface FeedImageItem {
+    text: string
+    url: string
+    target: string
+    title: string
 }
