7f877689aa59646ce293bfc518285c72b2da289f
Manh Hung Nguyen <manhhung.nguyen@episerver.com>
2023-11-09 09:57:50 +0700

    Move Form.tsx to form-react sdk
    Task: AFORM-3655

---
+++ src/@optimizely/forms-react/src/Form.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FormContainerBlock } from "./components/FormContainerBlock";
+import { UseFormLoaderProps, useFormLoader } from "./hooks/useFormLoader";
+import "./Form.scss"
+
+interface FormProps {
+    /**
+     * The form key that identifies the form
+     */
+    formKey: string,
+    /**
+     * The code of the form language
+     */
+    language: string | undefined,
+    /**
+     * The base url of Headless Form API
+     */
+    baseUrl: string | undefined
+}
+
+export const Form = ({formKey, language, baseUrl}: FormProps) => {
+  const {data: formData } = useFormLoader({ formKey, language, baseUrl } as UseFormLoaderProps)
+
+  return (
+    <>
+        {formData && <FormContainerBlock form={formData} key={formData.key} />}
+    </>
+  );
+}
