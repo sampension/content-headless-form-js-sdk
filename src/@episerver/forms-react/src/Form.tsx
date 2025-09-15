fc2bccf4651dfb2612cc25da33c939da9085bb7e
Simon Vu <svu@sampension.dk>
2025-09-15 15:17:13 +0200

    Fix return type

--- src/@episerver/forms-react/src/Form.tsx
+++ src/@episerver/forms-react/src/Form.tsx
@@ -36,7 +36,7 @@ interface FormProps {
   /**
    * Optional validation method to execute before allowing submission. Return true if submission should continue.
    */
-  onValidateBeforeSubmit?: () => Promise<boolean>;
+  onValidateBeforeSubmit?: (handleSubmit: (e:any) => void, handleSubmitArgs: any) => void;
   onLoadingChange?: (loading: boolean) => void;
   onError?: (error: unknown) => void;
 }
